1c6ce80fb37c3a639278220c01108343b470932b
youjong <youjong@users.noreply.github.com>
2024-07-03 15:01:38 +0900

    Ch6. 영화 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,65 +1,51 @@
-import PropTypes from 'prop-types';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Food({ name, picture, rating }) {
-	return (
-		<div>
-			<h2>I like {name}</h2>
-			<h4>{rating}/5.0</h4>
-			<img src={picture} alt={name} />
-		</div>
-	);
+class App extends React.Component {
+	state = {
+		isLoading: true,
+		movies: [],
+	};
+
+	getMovies = async () => {
+		const {
+			data: {
+				data: { movies },
+			},
+		} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+		this.setState({ movies, isLoading: false });
+	};
+	componentDidMount() {
+		this.getMovies();
+	}
+	render() {
+		const { isLoading, movies } = this.state;
+
+		return (
+			<section class='container'>
+				{isLoading ? (
+					<div class='loader'>
+						<span class='loader__text'>Loading...</span>
+					</div>
+				) : (
+					<div class='movies'>
+						{movies.map((movie) => (
+							<Movie
+								key={movie.id}
+								id={movie.id}
+								year={movie.year}
+								title={movie.title}
+								summary={movie.summary}
+								poster={movie.medium_cover_image}
+							/>
+						))}
+					</div>
+				)}
+			</section>
+		);
+	}
 }
 
-const foodLike = [
-	{
-		id: 1,
-		name: 'Kimchi',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Korean_Gimchi01.jpg/1200px-Korean_Gimchi01.jpg',
-		rating: 5,
-	},
-	{
-		id: 2,
-		name: 'Samgyeopsal',
-		image:
-			'https://i.namu.wiki/i/oFHlYDjoEh8f-cc3lNK9jAemRkbXxNGwUg7XiW5LGS6DF1P2x8GCeNQxbQhVIwtUS1u53YPw-uoyqpmLtrGNJA.webp',
-		rating: 4.9,
-	},
-	{
-		id: 3,
-		name: 'Bibimbap',
-		image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg',
-		rating: 4.5,
-	},
-	{
-		id: 4,
-		name: 'Dongasu',
-		image:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfEGlXYdegamf7pa_mxvqM1lXy3r6xdsw3Xw&s',
-		rating: 3.5,
-	},
-	{
-		id: 5,
-		name: 'Kimbap',
-		image:
-			'https://recipe1.ezmember.co.kr/cache/recipe/2022/09/30/8714be7bc9a0ed8525f007521485a80a1.jpg',
-		rating: 5.0,
-	},
-];
-
-function App() {
-	return (
-		<div>
-			{foodLike.map((dish) => (
-				<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-			))}
-		</div>
-	);
-}
-
-Food.propTypes = {
-	name: PropTypes.string.isRequired,
-	picture: PropTypes.string.isRequired,
-	rating: PropTypes.number,
-};
 export default App;
